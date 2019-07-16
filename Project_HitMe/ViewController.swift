0f4c5ead23c7cdb848fc4d95b2c518ceeef15cb3
Miloslav Milenkov <miloslav.milenkov@musala.com>
2019-07-16 16:34:20 +0300

    First Commit

--- Project_HitMe/ViewController.swift
+++ Project_HitMe/ViewController.swift
@@ -9,12 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var roundLabel: UILabel!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var guessSlider: UISlider!
+    
+    var score: Int = 0
+    var round: Int = 1
+    var answer: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        guessSlider.setThumbImage(UIImage(named: "SliderThumb-Normal"), for: .normal)
+        guessSlider.setThumbImage(UIImage(named: "SliderThumb-Highlighted"), for: .highlighted)
+        gameLoop()
     }
-
-
+    
+    @IBAction func hitMeTapped(_ sender: Any) {
+        incrementRound()
+        calculateScore()
+        let ac = UIAlertController(title: "Good job", message: "Your score is now: \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac,animated: true)
+        gameLoop()
+    }
+    
+    @IBAction func resetGame(_ sender: Any) {
+        score = 0
+        round = 1
+        roundLabel.text = "Round: \(round)"
+        scoreLabel.text = "Score: \(score)"
+    }
+    
+    func incrementRound() {
+        round += 1
+        roundLabel.text = "Round: \(round)"
+    }
+    
+    func calculateScore() {
+        var value = abs(answer - Int(guessSlider.value))
+        if (value == 0) {
+            value = 1
+        }
+        score += 200 / value
+        scoreLabel.text = "Score: \(score)"
+    }
+    
+    func gameLoop() {
+        answer = Int.random(in: 1...100)
+        titleLabel.text = "Put the Bull's Eye as close as you can to: \(answer)"
+    }
+    
 }
 

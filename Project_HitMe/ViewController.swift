cbc11cc6008aca1b4b40e663839cc9accd1b3c2f
Miloslav Milenkov <miloslav.milenkov@musala.com>
2019-07-16 13:53:15 +0300

    Initial Commit

---
+++ Project_HitMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project_HitMe
+//
+//  Created by Miloslav Milenkov on 16/07/2019.
+//  Copyright © 2019 Miloslav G. Milenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
